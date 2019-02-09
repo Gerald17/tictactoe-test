56b840b2e39f2d1ef063ae75edc042dd2ec1960e
gerardo_estrada <gerardo.estrada.dg@gmail.com>
2019-02-09 10:38:51 -0600

    creating html elements

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      
+        <h1>TIC TAC TOE</h1>
+
+        {/* Game grid 3x3 */}
+        <div className="game-grid">
+          <div className="grid-item">
+            <div className="figure o">1</div>
+          </div>
+          <div className="grid-item">
+            <div className="figure x">2</div>
+          </div>
+          <div className="grid-item">
+            <div className="figure o">3</div>
+          </div>
+        </div>
+
+        {/* players info */}
+        <div className="score-board">
+          <div className="player-info">
+            <div className="player-shape"></div>
+            <div className="player-name">Player Name</div>
+            <div className="player-score">Score <h4>3</h4></div>
+          </div>
+        </div>
+
       </div>
     );
   }
